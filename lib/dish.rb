b93dfa2c6d96fd58ba63def5e6b1db1821dd41ba
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-27 16:26:55 +0100

    Dish has a price

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,9 +1,11 @@
 class Dish
 
 	attr_accessor :name
+	attr_accessor :price
 
-	def initialize(name="surprise")
+	def initialize(name="surprise", price=0)
 		@name = name
+		@price = price
 	end
 
 end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -7,4 +7,8 @@ describe Dish do
 	it "should have a name" do
 		expect(dish.name).not_to be nil
 	end
+
+	it "should have a price" do
+		expect(dish.price.class).to eq(Fixnum)
+	end
 end
