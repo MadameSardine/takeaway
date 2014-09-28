e62355642b4157714efabb756840c9df90b65d08
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-28 11:33:57 +0100

    Corrected double syntax in menu spec

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,29 +2,31 @@ require 'menu'
 
 describe Menu do 
 
-	let(:menu) {Menu.new([:burger, :chicken])}
-	let(:dish) {double :dish}
+	let(:menu) {Menu.new}
+	let(:burger) {double :burger}
 
 	it "should have a list of dishes" do
-		expect(menu.dishes).not_to be nil
+		expect(menu.dish_count).to eq(0)
 	end
 
 	it "can have more dishes" do
-		expect{menu.add_dish(dish)}.to change{menu.dish_count}.by 1
+		expect{menu.add_dish(burger)}.to change{menu.dish_count}.by 1
 	end
 
 	it "can remove dishes" do
-		expect{menu.remove_dish(:burger)}.to change{menu.dish_count}.by -1
+		menu.add_dish(burger)
+		expect{menu.remove_dish(burger)}.to change{menu.dish_count}.by -1
 	end
 
 	it "has a new dish if added" do
-		menu.add_dish(:dish)
-		expect(menu.dishes.include?(:dish)).to be true
+		menu.add_dish(burger)
+		expect(menu.dishes.include?(burger)).to be true
 	end
 
 	it "doesn't have a dish if removed" do
-		menu.remove_dish(:burger)
-		expect(menu.dishes.include?(:burger)).to be false
+		menu.add_dish(burger)
+		menu.remove_dish(burger)
+		expect(menu.dishes.include?(burger)).to be false
 	end
 
 
