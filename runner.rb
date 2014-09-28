28f3ffd830ab568c7ed3840ce72d1d7cdab5eb8c
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-28 23:12:22 +0100

    Updated runner file

--- runner.rb
+++ runner.rb
@@ -4,30 +4,3 @@ require './lib/menu.rb'
 require './lib/order.rb'
 require './lib/customer.rb'
 require './lib/takeaway.rb'
-
-daMario = Takeaway.new(italian_menu)
-
-italian_menu = Menu.new([pizza, pasta, tiramisu, pannacotta, chianti, housewine, bruschetta, tricolore])
-
-pizza = Dish.new("Pizza", 11)
-pasta = Dish.new("Pasta", 13)
-tiramisu = Dish.new("Tiramisu", 5)
-pannacotta = Dish.new("Panacotta", 5)
-chianti = Dish.new("Chianti", 35)
-housewine = Dish.new("House wine", 18)
-bruschetta = Dish.new("Bruschetta", 8)
-tricolore = Dish.new("Tricolore", 8)
-
-sandrine = Customer.new("Sandrine", "07000000000")
-
-sandrinesorder = Order.new([starter, main, dessert1, dessert2, drink])
-
-starter = LineItem.new(bruschetta, 2)
-main = LineItem.new(pizza, 2)
-dessert1 = LineItem.new(pannacotta, 1)
-dessert2 = LineItem.new(tiramisu, 1)
-drink = LineItem.new(chianti, 1)
-
-daMario.take_order(sandrinesorder)
-# daMario.confirm_order(sandrinesorder)
-daMario.fulfill(sandrinesorder)
