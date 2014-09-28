33f562aeddb192deb3e75b18c0622aed673d84a8
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-28 12:10:12 +0100

    Customer can place an order

--- lib/customer.rb
+++ lib/customer.rb
@@ -6,4 +6,12 @@ class Customer
 		@orders = orders
 	end
 
+	def order_count
+		orders.count
+	end
+
+	def place_order(order)
+		orders << order
+	end
+	
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -3,8 +3,13 @@ require 'customer'
 describe Customer do
 
 	let(:customer) {Customer.new}
+	let(:order) {double :order}
 
 	it "should have no order" do
 		expect(customer.orders).to be_empty
+	end
+
+	it "can place an order" do
+		expect{customer.place_order(order)}.to change{customer.order_count}.by 1
 	end
 end
